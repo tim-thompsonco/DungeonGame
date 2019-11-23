2283b2880ac045a22d6320f36447619e2b881ab0
Tim Thompson <scubatim84@gmail.com>
2019-11-22 22:01:55 -0700

    Setting up repo to upload first part of game code

---
+++ DungeonGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DungeonGame
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("To be continued...");
+        }
+    }
+}
