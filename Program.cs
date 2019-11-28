02aef47346fb8e3af37b86551fc849090d825b96
Lord of Calzones <tim.thompsonco@gmail.com>
2019-11-28 10:21:55 -0700

    Moved room spawns to separate file to unclutter main program file, fixed bug with monster damage being nonexistent.

--- Program.cs
+++ Program.cs
@@ -4,45 +4,20 @@ using System.Collections.Generic;
 namespace DungeonGame {
   class MainClass {
     public static void Main(string[] args) {
-      // Establish game objects and game intro
-      Console.ForegroundColor = ConsoleColor.Gray;
-      Helper.GameIntro();
       // Game loading commands
+			Console.ForegroundColor = ConsoleColor.Gray;
+      Helper.GameIntro();
       var player = new NewPlayer(Helper.FetchPlayerName());
-      var room100 = new DungeonRoom(
-        "A dark room",
-        "You are in a dimly lit room. There is a lantern burning on a hook on the opposite wall. Water drips " +
-				"from a few stalagmites hanging from the ceiling, which is about 12 feet high. You appear to be in a " +
-				"dungeon. A moaning wail echoes in the distance through an open doorway in front of you. Glinting " +
-				"red eyes seem to be visible in the distance. There is an unsettling sound of a heavy metal object " +
-				"being dragged on the ground by a faint shape beyond the doorway. You can't make out what it is.",
-        0, // X coordinate
-				0, // Y coordinate
-				0, // Z coordinate
-        true, // goNorth bool
-        false); // goSouth bool
-      var room101 = new DungeonRoom(
-        "Dimly lit platform",
-        "Some other room...to be continued",
-        0, // X coordinate
-				1, // Y coordinate
-				0, // Z coordinate
-        new Monster("A rotting zombie", 25, 160, 1000, new Weapon("A notched axe", 10, 1.2)),
-        false, // goNorth bool
-        true); // goSouth bool
-      var spawnedRooms = new List<IRoom> {
-        room100,
-        room101
-      };
+			var spawnedRooms = new SpawnRooms().RetrieveSpawnRooms();
       // Set initial room condition
+			// On loading game, display room that player starts in
+      // Begin game by putting player in room 100
       var roomIndex = Helper.ChangeRoom(spawnedRooms, player, 0, 0, 0);
       spawnedRooms[roomIndex].LookRoom();
       // While loop to continue obtaining input from player
       while (true) {
         player.DisplayPlayerStats();
         spawnedRooms[roomIndex].ShowCommands();
-        // On loading game, display room that player starts in
-        // Begin game by putting player in room 100
         var input = Helper.GetFormattedInput();
         // Obtain player command and process command
         switch (input) {
---
+++ SpawnRooms.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace DungeonGame {
+	public class SpawnRooms {
+		public List<IRoom> _spawnedRooms { get; set; } = new List<IRoom>();
+
+		// Constructor
+		public SpawnRooms() {
+			var room100 = new DungeonRoom(
+				"A dark room",
+				"You are in a dimly lit room. There is a lantern burning on a hook on the opposite wall. Water drips " +
+				"from a few stalactites hanging from the ceiling, which is about 12 feet high. You appear to be in a " +
+				"dungeon. A moaning wail echoes in the distance through an open doorway in front of you. Glinting " +
+				"red eyes seem to be visible in the distance. There is an unsettling sound of a heavy metal object " +
+				"being dragged on the ground by a faint shape beyond the doorway. You can't make out what it is.",
+				0, // X coordinate
+				0, // Y coordinate
+				0, // Z coordinate
+				true, // goNorth bool
+				false); // goSouth bool
+			this._spawnedRooms.Add(room100);
+			var room101 = new DungeonRoom(
+				"Dimly lit platform",
+				"Some other room...to be continued",
+				0, // X coordinate
+				1, // Y coordinate
+				0, // Z coordinate
+				new Monster("rotting zombie", 25, 160, 1000, new Weapon("A notched axe", 25, 1.2)),
+				false, // goNorth bool
+				true); // goSouth bool
+			this._spawnedRooms.Add(room101);
+			}
+		// Method to retrieve room list
+		public List<IRoom> RetrieveSpawnRooms() {
+			return this._spawnedRooms;
+			}
+		}
+	}
