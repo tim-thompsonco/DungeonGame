dd0862303756ff79786e681458586c89002c50d3
Lord of Calzones <tim.thompsonco@gmail.com>
2020-03-14 11:56:32 -0600

    Fixed map border bug where undiscovered tile showed up as discovered in minimap

--- Handlers/OutputHandler.cs
+++ Handlers/OutputHandler.cs
@@ -103,7 +103,7 @@ namespace DungeonGame {
 									sameLineOutput.Add(Settings.FormatDefaultBackground()); // Background color
 									sameLineOutput.Add(Settings.GetLeftMapBorderSizeTwo()); // What prints to display
 									sameLineOutput.Add(Settings.FormatUpDownIndicator()); // Foreground color
-									sameLineOutput.Add(Settings.FormatDiscoveredTile()); // Background color
+									sameLineOutput.Add(Settings.FormatDefaultBackground()); // Background color
 									sameLineOutput.Add(Settings.GetUpDownMapTile()); // What prints to display
 								}
 								else {
@@ -111,14 +111,14 @@ namespace DungeonGame {
 									sameLineOutput.Add(Settings.FormatDefaultBackground()); // Background color
 									sameLineOutput.Add(Settings.GetLeftMapBorderSizeTwo()); // What prints to display
 									sameLineOutput.Add(Settings.FormatGeneralInfoText()); // Foreground color
-									sameLineOutput.Add(Settings.FormatDiscoveredTile()); // Background color
+									sameLineOutput.Add(Settings.FormatDefaultBackground()); // Background color
 									sameLineOutput.Add(Settings.GetEmptyMapTileSizeTwo()); // What prints to display
 								}
 							}
 							else if (j == endRightPos) {
 								if (RoomHandler.Rooms[roomIndex].GoUp || RoomHandler.Rooms[roomIndex].GoDown) {
 									sameLineOutput.Add(Settings.FormatUpDownIndicator()); // Foreground color
-									sameLineOutput.Add(Settings.FormatDiscoveredTile()); // Background color
+									sameLineOutput.Add(Settings.FormatDefaultBackground()); // Background color
 									sameLineOutput.Add(Settings.GetUpDownMapTile()); // What prints to display
 									sameLineOutput.Add(Settings.FormatUpDownIndicator()); // Foreground color
 									sameLineOutput.Add(Settings.FormatDefaultBackground()); // Background color
@@ -126,7 +126,7 @@ namespace DungeonGame {
 								}
 								else {
 									sameLineOutput.Add(Settings.FormatGeneralInfoText()); // Foreground color
-									sameLineOutput.Add(Settings.FormatDiscoveredTile()); // Background color
+									sameLineOutput.Add(Settings.FormatDefaultBackground()); // Background color
 									sameLineOutput.Add(Settings.GetEmptyMapTileSizeTwo()); // What prints to display
 									sameLineOutput.Add(Settings.FormatGeneralInfoText()); // Foreground color
 									sameLineOutput.Add(Settings.FormatDefaultBackground()); // Background color
