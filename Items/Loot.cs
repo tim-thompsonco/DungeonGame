1d1fd6a1787f77e7ef514a9ed95d336c199a126c
Tim Thompson <scubatim84@gmail.com>
2020-12-10 19:07:44 -0700

    Refactor Loot.cs

--- Items/Loot.cs
+++ Items/Loot.cs
@@ -22,41 +22,41 @@
 		public bool _Equipped { get; set; }
 		public int _ItemValue { get; set; }
 		public int _Weight { get; set; }
-		public GemType GemCategory { get; set; }
-		public GemLevel GemStrength { get; set; }
+		public GemType _GemCategory { get; set; }
+		public GemLevel _GemStrength { get; set; }
 
 		// Default constructor for JSON serialization to work since there isn't 1 main constructor
 		public Loot() { }
 		public Loot(string name, int level, int weight)
 		{
-			this._Name = name;
-			this._Weight = weight;
-			this._ItemValue = 15 + (level - 1) * 5;
-			this._Desc = "A " + this._Name + " that is worth some money to the right vendor.";
+			_Name = name;
+			_Weight = weight;
+			_ItemValue = 15 + ((level - 1) * 5);
+			_Desc = $"A {_Name} that is worth some money to the right vendor.";
 		}
 		public Loot(int level, GemType gemType)
 		{
-			this.GemCategory = gemType;
-			this._Weight = 1;
-			this._ItemValue = level * 20;
+			_GemCategory = gemType;
+			_Weight = 1;
+			_ItemValue = level * 20;
 			string name;
 			if (level <= 3)
 			{
-				this.GemStrength = GemLevel.Chipped;
-				name = GemLevel.Chipped + " " + gemType;
+				_GemStrength = GemLevel.Chipped;
+				name = $"{GemLevel.Chipped} {gemType}";
 			}
 			else if (level <= 6)
 			{
-				this.GemStrength = GemLevel.Dull;
-				name = GemLevel.Dull + " " + gemType;
+				_GemStrength = GemLevel.Dull;
+				name = $"{GemLevel.Dull} {gemType}";
 			}
 			else
 			{
-				this.GemStrength = GemLevel.Normal;
+				_GemStrength = GemLevel.Normal;
 				name = gemType.ToString();
 			}
-			this._Name = name.ToLower();
-			this._Desc = "A " + this._Name + " that is worth some money to the right vendor.";
+			_Name = name.ToLower();
+			_Desc = $"A {_Name} that is worth some money to the right vendor.";
 		}
 	}
 }
