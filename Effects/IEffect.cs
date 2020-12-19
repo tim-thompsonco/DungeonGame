b043f99cd5a2b0e6e29ac1f760519908f42a5f40
Tim Thompson <scubatim84@gmail.com>
2020-12-19 10:08:03 -0700

    Create IEffect interface, create OnFireEffect class

---
+++ Effects/IEffect.cs
@@ -0,0 +1,5 @@
+﻿namespace DungeonGame.Effects {
+	public interface IEffect : IName {
+		bool _IsEffectExpired { get; set; }
+	}
+}
---
+++ Effects/OnFireEffect.cs
@@ -0,0 +1,89 @@
+﻿using DungeonGame.Controllers;
+using DungeonGame.Monsters;
+using DungeonGame.Players;
+
+namespace DungeonGame.Effects {
+	public class OnFireEffect : IEffect {
+		public string _Name { get; set; }
+		public bool _IsEffectExpired { get ; set; }
+		private readonly int _FireDamageOverTime;
+		private int _CurrentTick;
+		private readonly int _MaxTick;
+
+		public OnFireEffect(string name, int fireDamageOverTime, int maxTick) {
+			_Name = name;
+			_FireDamageOverTime = fireDamageOverTime;
+			_CurrentTick = 1;
+			_MaxTick = maxTick;
+		}
+
+		public void OnFireTick(Monster monster) {
+			if (_IsEffectExpired) {
+				return;
+			}
+
+			DecreaseHealthFromBurnDamage(monster);
+
+			string burnMessage = GetBurnMessage(monster);
+			DisplayBurnMessage(burnMessage);
+
+			IncrementCurrentTick();
+
+			if (_CurrentTick > _MaxTick) {
+				SetEffectAsExpired();
+			}
+		}
+
+		public void OnFireTick(Player player) {
+			if (_IsEffectExpired) {
+				return;
+			}
+
+			DecreaseHealthFromBurnDamage(player);
+
+			string burnMessage = GetBurnMessage();
+			DisplayBurnMessage(burnMessage);
+
+			IncrementCurrentTick();
+
+			if (_CurrentTick > _MaxTick) {
+				SetEffectAsExpired();
+			}
+		}
+
+		private Monster DecreaseHealthFromBurnDamage(Monster monster) {
+			monster._HitPoints -= _FireDamageOverTime;
+
+			return monster;
+		}
+
+		private Player DecreaseHealthFromBurnDamage(Player player) {
+			player._HitPoints -= _FireDamageOverTime;
+
+			return player;
+		}
+
+		private string GetBurnMessage(Monster monster) {
+			return $"The {monster._Name} burns for {_FireDamageOverTime} fire damage.";
+		}
+
+		private string GetBurnMessage() {
+			return $"You burn for {_FireDamageOverTime} fire damage.";
+		}
+
+		private void DisplayBurnMessage(string burnMessage) {
+			OutputController.Display.StoreUserOutput(
+				Settings.FormatOnFireText(),
+				Settings.FormatDefaultBackground(),
+				burnMessage);
+		}
+
+		private void IncrementCurrentTick() {
+			_CurrentTick++;
+		}
+
+		private void SetEffectAsExpired() {
+			_IsEffectExpired = true;
+		}
+	}
+}
