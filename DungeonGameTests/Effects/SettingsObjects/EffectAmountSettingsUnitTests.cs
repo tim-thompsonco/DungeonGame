4675c3f8ad62d2b08a3e875245d0878426b45057
Tim Thompson <scubatim84@gmail.com>
2021-05-08 10:45:42 -0600

    Add EffectAmountSettings class, add unit tests for EffectAmountSettings

---
+++ DungeonGameTests/Effects/SettingsObjects/EffectAmountSettingsUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using DungeonGame.Effects.SettingsObjects;
+using DungeonGame.Players;
+using NUnit.Framework;
+using System;
+
+namespace DungeonGameTests.Effects.SettingsObjects {
+	internal class EffectAmountSettingsUnitTests {
+		[Test]
+		public void EffectAmountSettingsMissingAmountThrowsException() {
+			Player player = new Player("test", Player.PlayerClassType.Mage);
+			EffectAmountSettings effectSettings = new EffectAmountSettings {
+				EffectHolder = player,
+				Name = "test"
+			};
+			const string expectedErrorMessage = "EffectAmountSettings: Amount has not been set.";
+
+			Exception exception = Assert.Throws<Exception>(() => effectSettings.ValidateSettings());
+
+			Assert.AreEqual(expectedErrorMessage, exception.Message);
+		}
+
+		[Test]
+		public void EffectAmountSettingsMissingEffectHolderThrowsException() {
+			EffectAmountSettings effectSettings = new EffectAmountSettings {
+				Amount = 50,
+				Name = "test"
+			};
+			const string expectedErrorMessage = "EffectAmountSettings: EffectHolder has not been set.";
+
+			Exception exception = Assert.Throws<Exception>(() => effectSettings.ValidateSettings());
+
+			Assert.AreEqual(expectedErrorMessage, exception.Message);
+		}
+
+		[Test]
+		public void EffectAmountSettingsMissingNameThrowsException() {
+			Player player = new Player("test", Player.PlayerClassType.Mage);
+			EffectAmountSettings effectSettings = new EffectAmountSettings {
+				Amount = 50,
+				EffectHolder = player,
+			};
+			const string expectedErrorMessage = "EffectAmountSettings: Name has not been set.";
+
+			Exception exception = Assert.Throws<Exception>(() => effectSettings.ValidateSettings());
+
+			Assert.AreEqual(expectedErrorMessage, exception.Message);
+		}
+
+		[Test]
+		public void EffectAmountSettingsWithAllSettingsDoesNotThrowException() {
+			Player player = new Player("test", Player.PlayerClassType.Mage);
+			EffectAmountSettings effectSettings = new EffectAmountSettings {
+				Amount = 50,
+				EffectHolder = player,
+				Name = "test"
+			};
+
+			Assert.DoesNotThrow(() => effectSettings.ValidateSettings());
+		}
+	}
+}
---
+++ Effects/SettingsObjects/EffectAmountSettings.cs
@@ -0,0 +1,24 @@
+﻿using DungeonGame.Interfaces;
+using System;
+
+namespace DungeonGame.Effects.SettingsObjects {
+	public class EffectAmountSettings : IEffectSettings {
+		public int? Amount { get; set; }
+		public IEffectHolder EffectHolder { get; set; }
+		public string Name { get; set; }
+
+		public void ValidateSettings() {
+			if (Amount is null) {
+				throw new Exception("EffectAmountSettings: Amount has not been set.");
+			}
+
+			if (EffectHolder is null) {
+				throw new Exception("EffectAmountSettings: EffectHolder has not been set.");
+			}
+
+			if (Name is null) {
+				throw new Exception("EffectAmountSettings: Name has not been set.");
+			}
+		}
+	}
+}
